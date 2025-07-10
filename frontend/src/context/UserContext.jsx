800762fdea01a7dcca624479bf40cffac5cbed3a
vraj62023 <VRAJ62023.24@KGPIAN.IITKGP.AC.IN>
2025-07-10 21:39:58 +0530

    Update UserContext.jsx

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 
 function UserContext({ children }) {
-    const serverUrl = "http://localhost:8000"
+    const serverUrl = "https://virtualassistantbackend-gcni.onrender.com"
     const [userData, setUserData] = useState(null)
     const [frontendImage, setFrontendImage] = useState(null)
     const [backendImage, setBackendImage] = useState(null)
